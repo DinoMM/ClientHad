a2344e94f0ecfd35350bac0a2395091e3c6ad230
DinoMM <dado2002.tablet@gmail.com>
2024-01-02 13:27:02 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ClientHad.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClientHad.dir/main.cpp.o"
+  "ClientHad"
+  "ClientHad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClientHad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
